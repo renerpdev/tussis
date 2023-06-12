1a6263296753f844d4a9ba2306a902e7cdce7b4c
René Ricardo <32307287+renericardoperez@users.noreply.github.com>
2023-06-12 15:47:22 -0300

    add firebase functions and hosting deployments

--- packages/back-office/src/main.ts
+++ packages/back-office/src/main.ts
@@ -1,24 +1,23 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app/app.module';
+import express from 'express';
+import * as functions from 'firebase-functions';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
-  });
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+const server = express();
+
+export const createNestServer = async (expressInstance) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance)
   );
-}
 
-bootstrap();
+  return app.init();
+};
+
+createNestServer(server)
+  .then((v) => console.log('Nest Ready'))
+  .catch((err) => console.error('Nest broken', err));
+
+// Connect express server to Firebase Functions
+export const api = functions.https.onRequest(server);
