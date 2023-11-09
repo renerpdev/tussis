c4baae100e27ac3a52de8c9a7a83dded3bfd0bda
René Ricardo <32307287+renericardoperez@users.noreply.github.com>
2023-11-09 18:15:56 -0300

    verify MODE and setup firebase app base upon

--- packages/back-office/src/app/auth/firebase-auth.strategy.ts
+++ packages/back-office/src/app/auth/firebase-auth.strategy.ts
@@ -4,6 +4,8 @@ import * as firebase from 'firebase-admin'
 import { ExtractJwt, Strategy } from 'passport-firebase-jwt'
 import { FirebaseAuthProvider } from './auth.providers'
 
+const MODE = process.env.ENV
+
 @Injectable()
 export class FirebaseAuthStrategy extends PassportStrategy(Strategy, 'firebase-auth') {
   private defaultApp: any
@@ -12,22 +14,16 @@ export class FirebaseAuthStrategy extends PassportStrategy(Strategy, 'firebase-a
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     })
-    if (auth.keyFilename) {
-      const firebaseConfig: any = import(auth.keyFilename).then(() => {
-        const params = {
-          type: firebaseConfig.type,
-          projectId: firebaseConfig.project_id,
-          privateKeyId: firebaseConfig.private_key_id,
-          privateKey: firebaseConfig.private_key,
-          clientEmail: firebaseConfig.client_email,
-          clientId: firebaseConfig.client_id,
-          authUri: firebaseConfig.auth_uri,
-          tokenUri: firebaseConfig.token_uri,
-          authProviderX509CertUrl: firebaseConfig.auth_provider_x509_cert_url,
-          clientC509CertUrl: firebaseConfig.client_x509_cert_url,
-        }
-        this.defaultApp = firebase.initializeApp(params)
-      })
+    if (MODE === 'development') {
+      if (auth.keyFilename) {
+        import(auth.keyFilename).then(serviceAccount => {
+          this.defaultApp = firebase.initializeApp({
+            credential: firebase.credential.cert(serviceAccount),
+          })
+        })
+      }
+    } else {
+      this.defaultApp = firebase.initializeApp()
     }
   }
 
