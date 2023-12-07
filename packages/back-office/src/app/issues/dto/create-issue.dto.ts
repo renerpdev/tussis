87e33e7b8b31d5363a185f6b8b6c687e8cab0aa5
René Ricardo <32307287+renericardoperez@users.noreply.github.com>
2023-12-07 12:24:03 -0300

    make symptoms to be required for creating new issues

--- packages/back-office/src/app/issues/dto/create-issue.dto.ts
+++ packages/back-office/src/app/issues/dto/create-issue.dto.ts
@@ -1,12 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger'
 import { Type } from 'class-transformer'
-import { IsArray, IsDate, IsOptional, IsString } from 'class-validator'
+import { ArrayMinSize, IsArray, IsDate, IsOptional, IsString } from 'class-validator'
 
 export class CreateIssueDto {
   @ApiProperty()
-  @IsOptional()
   @IsArray()
-  symptoms?: string[]
+  @ArrayMinSize(1)
+  symptoms: string[]
 
   @ApiProperty()
   @IsOptional()
